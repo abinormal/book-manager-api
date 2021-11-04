0f4799344e698e169cccd15414e7a7bd0916dbcf
Abi Sidford <asidford@gmail.com>
2021-11-04 09:51:01 +0000

    Added API intro information.

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -24,6 +26,16 @@ public class BookmanagerApplication {
 				.select()
 				.paths(PathSelectors.any())
 				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.build()
+				.apiInfo(apiInformation());
+	}
+
+	private ApiInfo apiInformation(){
+		return new ApiInfoBuilder()
+				.title("Welcome to the fabulous Minimalist Book Manager API!")
+				.description("Overflowing bookcases? Love book shops but can't remember what you have in your library? " +
+						"Then this API is for you!")
+				.version("v1")
 				.build();
 	}
 
